50cf02b9c6503009425e2113302ed5da55f82aae
Is Teamoor <is.teamoor@gmail.com>
2024-04-08 14:28:15 +0500

    bids table

--- app.py
+++ app.py
@@ -52,10 +52,12 @@ def createLot():
     img.save(os.path.join("server/images/compressed", compressed_filename), format="JPEG", quality=50)
 
     ### Создание записей в бд
-    newLot = query('set', f"INSERT INTO lots (label, description, img) VALUES ('{label}', '{description}', '{filename}')", 'lots')
-    #newBid = query('set', f"INSERT INTO bids (lot_id, user_id, price) VALUES ('{newLot[0]}', '{user_id}', '0')", 'bids')
+    command = 'set', f"INSERT INTO lots (label, description, img) VALUES ('{label}', '{description}', '{filename}')"
+    print(command)
+    """newLot = query(command, 'lots')
+    newBid = query('set', f"INSERT INTO bids (lot_id, user_id, price) VALUES ('{newLot[0]}', '{user_id}', '0')", 'bids')
     print( newLot)
-    #print( newBid)
+    print( newBid)"""
 
     return jsonify({'creating new lot': 'success'})
 
